3fc0fef6d9d74a9585c38cd4e6684f5ce7c978be
Eddie Madrigal <edmadrigal@yahoo.com>
2020-04-27 08:55:59 -0700

    users-router.js file refactored

--- users/users-router.js
+++ users/users-router.js
@@ -17,28 +17,20 @@ router.post('/register', (req, res) => {
     res.status(400);
     res.send('Invalid details');
   } else { 
-    Users.find(user => {e
+    Users.filter(user => {
       if (user.username === req.body.username) {
         res.json({ message: 'User already exists. Login or register.' })
       } else {
         Users.add(req.body)
           .then(saved => {
             res.status(201).json({ message: 'user added!'});
-            // req.session.user = newUser;
-            // res.redirect('/dad-jokes')
           })
           .catch(error => {
             res.status(500).json(error)
-    })
+          })
       }
-    });
-    // const newUser = { username: req.body.username, password: req.body.password }
-    
-  }
-  // let user = req.body;
-  // const hash = bcrypt.hashSync(user.password, 10); 
-  // user.password = hash;
-  
+    });    
+  }  
 });
 
 router.post('/login', (req, res) => {
