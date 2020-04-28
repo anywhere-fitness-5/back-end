2ec729b8a4b464932d849af16d1f78bfd5d162b4
Eddie Madrigal <edmadrigal@yahoo.com>
2020-04-27 17:29:15 -0700

    migration files refactored

--- data/migrations/20200427151330_create-fitness-db.js
+++ data/migrations/20200427151330_create-fitness-db.js
@@ -1,25 +1,8 @@
 
 exports.up = function(knex) {
-  return knex.schema
 
-  // users table
-  // .createTable('users', users => {
-  //   users
-  //     .increments();
-  //   users
-  //     .string('username', 30)
-  //     .unique()
-  //     .notNullable();      
-  //   users
-  //     .string('password', 128)
-  //     .notNullable();
-  //   users
-  //     .string('fname', 30);
-  //   users
-  //     .string('lname', 30);
-  // })
 };
 
 exports.down = function(knex) {
-  // return knex.schema.dropTableIfExists('users');
+
 };
