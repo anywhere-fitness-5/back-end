1e3a8c252a2c2e2b83bd5d3eeb76e562027ef7d8
JW Nicholson <john.nicholson812@gmail.com>
2020-04-29 15:02:18 -0400

    updated server.js

--- api/server.js
+++ api/server.js
@@ -2,8 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 
-const usersRouter = require('../users/users-router.js');
-
+const usersRouter = require('../users/users-router');
+const classesRouter = require('../classes/classes-router');
 const server = express();
 
 server.use(helmet());
@@ -11,9 +11,13 @@ server.use(cors());
 server.use(express.json());
 
 server.use('/api/users', usersRouter);
+//server.use('https://aw-fitness-5.herokuapp.com/api/users', usersRouter);
+
+//server.use('/api/classes', classesRouter);
+server.use('https://aw-fitness-5.herokuapp.com/api/classes', classesRouter);
 
 server.use('/', (req, res) => {
-  res.send("API up ...");
+  res.send("=== API is running ===");
 }) 
 
 module.exports = server;
