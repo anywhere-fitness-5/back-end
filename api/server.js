67bde51038debf284c9d9adc629f3bfa6c6bb0f5
JW Nicholson <john.nicholson812@gmail.com>
2020-04-29 15:19:00 -0400

    setting up add classes routes

--- api/server.js
+++ api/server.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
 const cors = require('cors');
 const helmet = require('helmet');
 
 const usersRouter = require('../users/users-router');
 const classesRouter = require('../classes/classes-router');
+const secureAddClassRouter = require('../secure/addClassesRouter');
+
 const server = express();
 
 server.use(helmet());
@@ -13,8 +16,10 @@ server.use(express.json());
 server.use('/api/users', usersRouter);
 //server.use('https://aw-fitness-5.herokuapp.com/api/users', usersRouter);
 
-//server.use('/api/classes', classesRouter);
-server.use('https://aw-fitness-5.herokuapp.com/api/classes', classesRouter);
+server.use('/api/classes', classesRouter);
+//server.use('https://aw-fitness-5.herokuapp.com/api/classes', classesRouter);
+
+server.use('/api/addClass', secureAddClassRouter);
 
 server.use('/', (req, res) => {
   res.send("=== API is running ===");
