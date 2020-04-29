1916e6a919c5cef7cc4387714b11460e85688058
JW Nicholson <john.nicholson812@gmail.com>
2020-04-29 14:59:27 -0400

    added classes route

--- api/server.js
+++ api/server.js
@@ -2,8 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 
-const usersRouter = require('../users/users-router.js');
-
+const usersRouter = require('../users/users-router');
+const classesRouter = require('../classes/classes-router');
 const server = express();
 
 server.use(helmet());
@@ -12,6 +12,9 @@ server.use(express.json());
 
 server.use('/api/users', usersRouter);
 //server.use('https://aw-fitness-5.herokuapp.com/api/users', usersRouter);
+
+//server.use('/api/classes', classesRouter);
+server.use('https://aw-fitness-5.herokuapp.com/api/classes', classesRouter);
 
 server.use('/', (req, res) => {
   res.send("=== API is running ===");
---
+++ classes/classes-router.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+const Classes = require('./classes-model');
+
+router.get('/', (req, res) => {
+    Classes.find()
+      .then(classes => {
+        res.json(classes)
+      })
+      .catch(err => res.send(err));
+  })
+
+
+  module.exports = router;
